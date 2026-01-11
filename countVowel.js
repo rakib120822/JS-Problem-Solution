331e431f08a951508eea64d7c6271bd621e36924
rakib120822 <payelrakibulislam@gmail.com>
2026-01-11 12:11:59 +0600

    change in count vowel problem

--- countVowel.js
+++ countVowel.js
@@ -1,8 +1,9 @@
 let str = 'programming';
 
 let count = 0;
+let allVowels = 'aeiouAEIOU';
 for(let i = 0; i < str.length; i++) {
-    if('aeiouAEIOU'.includes(str[i])) {
+    if(allVowels.includes(str[i])) {
         count++;
     }   
 }
