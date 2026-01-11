9667b900b31bd98d0fc3ea729f8f92c608547921
rakib120822 <payelrakibulislam@gmail.com>
2026-01-11 11:28:18 +0600

    complete count vowel problem

---
+++ countVowel.js
@@ -0,0 +1,10 @@
+let str = 'programming';
+
+let count = 0;
+for(let i = 0; i < str.length; i++) {
+    if('aeiouAEIOU'.includes(str[i])) {
+        count++;
+    }   
+}
+
+console.log(count);        
